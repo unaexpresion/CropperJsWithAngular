508e8cf8f1d0179d21a3e8921b4fd6d20d115a03
Carlos Medina <xziolhvinvhgl@gmail.com>
2017-11-15 12:44:43 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+
+import * as Cropper from 'cropperjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+  @ViewChild('photo')
+  private elPhoto: ElementRef;
+
+  @ViewChild('result')
+  private elResult: ElementRef;
+
+  private cropper;
+  private result;
+  private data;
+
+  ngOnInit() {
+    var image = this.elPhoto.nativeElement;
+    this.result = this.elResult.nativeElement;
+
+    var croppable = false;
+    this.cropper = new Cropper(image, {
+      aspectRatio: 1 / 1,
+      viewMode: 1,
+      autoCrop: false,
+      minCropBoxWidth: 100,
+      minCropBoxHeight: 100,
+      ready: function () {
+        croppable = true;
+        this.cropper.crop();
+        this.cropper.setCropBoxData({
+            width: 150,
+            height: 150
+        });
+      }
+    });
+  }
+
+  makeCrop() {
+    var img = this.cropper.getCroppedCanvas({
+      width: 250,
+      height: 250
+    });
+    this.data = img.toDataURL();
+    this.result.src = this.data;
+  }
+
 }
